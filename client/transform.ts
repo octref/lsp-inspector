463470c9c97e83c8db62d334602e57154fe4fd3e
Pine Wu <octref@gmail.com>
2018-06-12 12:54:05 -0700

    Update transform schema

--- client/transform.ts
+++ client/transform.ts
@@ -1,21 +1,29 @@
 export interface LspItem {
-  time: string;
-  msg: string;
-  msgKind: string;
-  msgType: string;
-  msgId: string;
-  msgLatency: string;
-  arg: any;
+  time: string
+  msg: string
+  msgKind: string
+  msgType: string
+  msgId: string
+  msgLatency: string
+  arg: any
 }
 
 export function transform(input: string): LspItem[] {
   const logs = input.split('\n\n\n')
 
-  return logs
+  const arr: LspItem[] = []
+  logs
     .filter(l => {
       return l.startsWith('[Trace')
     })
-    .map(parseRawLog)
+    .forEach(l => {
+      const parsed = parseRawLog(l)
+      if (parsed) {
+        arr.push(parsed)
+      }
+    })
+
+  return arr
 }
 
 function parseRawLog(log: string) {
@@ -31,11 +39,15 @@ function parseRawLog(log: string) {
     const [jsonIdentifier, restOfFirstLineJson] = reJsonFirstLine.exec(firstLineJson).slice(1)
     const parsedArg = JSON.parse([restOfFirstLineJson, ...rawLogs.slice(2)].join('\n'))
 
-    return {
-      time,
-      msg,
-      ...extractMsg(msg),
-      arg: parsedArg
+    try {
+      return {
+        time,
+        msg,
+        ...extractMsg(msg),
+        arg: parsedArg
+      }
+    } catch (e) {
+      return null
     }
   }
 }
@@ -44,24 +56,29 @@ function extractMsg(msg: string) {
   let msgKind
 
   if (msg.startsWith('Sending notification')) {
-    msgKind = 'notification'
+    msgKind = 'send-notification'
   } else if (msg.startsWith('Sending request')) {
     msgKind = 'request'
-  } else {
+  } else if (msg.startsWith('Received response')) {
     msgKind = 'response'
+  } else {
+    msgKind = 'receive-notification'
   }
 
-  const reNotification = /Sending notification '(.*)'/
+  const reSendNotification = /Sending notification '(.*)'/
   const reRequest = /Sending request '(.*) - \((\d+)\)'/
   const reResponse = /Received response '(.*) - \((\d+)\)' in (\d+ms)/
+  const reReceiveNotification = /Received notification '(.*)'/
 
   let msgType, msgId, msgLatency
-  if (msgKind === 'notification') {
-    [msgType] = reNotification.exec(msg).slice(1)
+  if (msgKind === 'send-notification') {
+    ;[msgType] = reSendNotification.exec(msg).slice(1)
   } else if (msgKind === 'request') {
-    [msgType, msgId] = reRequest.exec(msg).slice(1)
+    ;[msgType, msgId] = reRequest.exec(msg).slice(1)
+  } else if (msgKind === 'response') {
+    ;[msgType, msgId, msgLatency] = reResponse.exec(msg).slice(1)
   } else {
-    [msgType, msgId, msgLatency] = reResponse.exec(msg).slice(1)
+    ;[msgType] = reReceiveNotification.exec(msg).slice(1)
   }
 
   return {
